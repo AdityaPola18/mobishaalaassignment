4cb3babc74fd83d96828d02ffe6d696419a648da
Adityapola <adityapola124@gmail.com>
2024-03-15 12:16:09 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,139 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="container">
+
+      <div class="row bg-container">
+
+        <div class="msg-cont">
+
+          <h1 class="heading">Mobishaala</h1>
+
+          <h2 class="heading2">
+
+          " Create your "
+
+          <span class="span1">Digital Classroom</span>
+
+          " in 5 mins for "
+
+          <span class="span1">FREE</span>
+
+          " & connect to 1+ Million Students. "
+
+          </h2>
+          <ul class="listcont"> == $0
+
+            <li class="listItem">No technical knowledge required</li>
+
+            <li class="listItem">Quickly setup live class, tests, notes, ebooks and more</li>
+
+            <li class="listItem">Reach out to millions of students anywhere, anytime</li>
+          </ul>
+          <div class="buttons-cont">
+
+            <div class="buttons"> 
+
+            <h4 class="btnHeading2">Institute/Teacher</h4>
+            <button type="button" class="btn">Get Started</button>
+
+            </div>
+
+
+            <div class="buttons">
+
+              <h4 class="btnHeading2">Student</h4>
+
+              <button type="button" class="btn">Download App</button>
+
+            </div>
+
+            <button class="contactBtn"> flex == $0
+
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 48 48"></svg>
+
+              "Talk to us"
+
+            </button>
+
+            </div>
+
+          </div>
+          <div class="videoCont">
+
+            <iframe class="video" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe> <button class="btn">Buy</button>
+
+          </div>
+          <div class="buttons-cont2"> 
+
+            <div class="buttons"> 
+
+            <h4 class="btnHeading">Institute/Teacher</h4>
+
+            <button type="button" class="btn">Get Started</button>
+
+            </div>
+
+            <div class="buttons"> flex
+
+            <h4 class="btnHeading">Student</h4>
+
+            <button type="button" class="btn">Download App</button>
+
+            </div>
+
+            <button class="contactBtn">
+
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 48 48"></svg>
+
+              "Talk to us"
+
+            </button>
+
+            </div>
+            <div class="card">
+
+              <h1 class="h1">Mobishaala</h1>
+
+              <h2 class="cardHeading">Vision</h2>
+
+              <p class="p1"></p>
+
+              <h1 class="cardHeading">Address</h1>
+
+              <p class="p1">
+
+              <span class="span2">Registered Office:</span>
+
+              804, 5th Cross, 9th Main, 4th Block, Koramangala, Bangalore, Karnataka, 560034"
+
+              </p>
+
+              <p class="p1">
+
+              <span class="span2">Corporate Office:</span>
+
+              "293, Westend Marg, Near Saket, Saidulajab, Saket, New Delhi, 110030"
+
+              </p>
+
+              <h1 class="cardHeading">Quick Links</h1>
+
+              <p class="p1">Blog</p>
+
+              <p class="p1">Android App</p>
+
+              <p class="p1"> IOS App</p>
+
+              </div>
+
+              </div>
+
+              </div>
+      
+
   );
 }
 
